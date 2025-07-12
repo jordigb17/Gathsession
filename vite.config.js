608265255dd9d1a85e19e1c913a24263b4472c96
Jordi <jordigb_17@proton.me>
2025-07-12 17:26:07 +0200

    Preparando despliegue

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/nombre_repo/',
+  base: '/Gathsession/',
   server: {
     watch: {
       usePolling: true, //  Fuerza Vite a hacer polling en lugar de eventos del SO
